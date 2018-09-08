d5edce247d53de82d840a46868050f1764b19b81
Edgar Sánchez <sanchez82@uabc.edu.mx>
2018-09-08 08:48:43 -0700

    Archivos para inicializar proyecto

---
+++ main.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow} = require('electron')
+  
+  let win
+  
+  function createWindow () {
+
+    win = new BrowserWindow({width: 1410, height: 820})
+  
+    win.loadFile('index.html')
+
+    win.on('closed', () => {
+
+      win = null
+    })
+  }
+
+  app.on('ready', createWindow)
+
+  app.on('window-all-closed', () => {
+
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+  
+  app.on('activate', () => {
+    if (win === null) {
+      createWindow()
+    }
+  })
