3248f701d6352a169fe573af3e6eeba1a8484740
eduardo <eduardo.escalona@lkmx.io>
2018-09-09 00:27:44 -0700

    routes to load specific view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
+import {NgModule} from "@angular/core";
+import {LoginComponent} from "./views/login/login.component";
+import {AppComponent} from "./app.component";
+import {FormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
+import {routing} from "./app.routing.module";
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        LoginComponent
+    ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/views/baptism/bautismo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bautismo',
+  templateUrl: './bautismo.component.html',
+  styleUrls: ['./bautismo.component.css']
+})
+export class BautismoComponent {
+  title = 'angular-electron';
+}
